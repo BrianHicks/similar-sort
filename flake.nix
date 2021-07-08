87ff983fcecdcf9f53ba9cb06c4f2298ce27cd94
Brian Hicks <brian@brianthicks.com>
2021-07-08 15:54:18 -0500

    convert to a flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "sort lines by their similarity to a candidate string";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=release-21.05";
+    gitignore = {
+      url = "github:hercules-ci/gitignore";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        gitignore = pkgs.callPackage inputs.gitignore { };
+      in rec {
+        packages.similar-sort = pkgs.stdenv.mkDerivation {
+          name = "similar-sort";
+          buildInputs = [ pkgs.go ];
+          src = gitignore.gitignoreSource ./.;
+
+          buildPhase = ''
+            env HOME=$(pwd) GOPATH=$(pwd) go build similar-sort.go
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp similar-sort $out/bin
+          '';
+        };
+
+        defaultPackage = packages.similar-sort;
+      });
+}
