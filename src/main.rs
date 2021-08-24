96306f841dc4c3ab65adcb0654d86bfdbb02ba35
Brian Hicks <brian@brianthicks.com>
2021-08-24 15:48:26 -0500

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{crate_authors, crate_version, App, Arg};
 use color_eyre::eyre::{ContextCompat, Result, WrapErr};
+use rayon::prelude::*;
 use std::io::{self, stdin, BufRead};
+use strsim::levenshtein;
 
 fn main() {
     if let Err(err) = try_main() {
@@ -27,13 +29,17 @@ fn try_main() -> Result<()> {
         "could not get the target value. This is an internal error and should be reported.",
     )?;
 
-    let lines: Vec<String> = stdin()
+    let mut lines: Vec<String> = stdin()
         .lock()
         .lines()
         .collect::<io::Result<Vec<String>>>()
         .context("could not read lines from stdin")?;
 
-    println!("{:?}", lines);
+    lines.par_sort_by_key(|candidate| levenshtein(target, candidate));
+
+    for candidate in lines {
+        println!("{}", candidate);
+    }
 
     Ok(())
 }
