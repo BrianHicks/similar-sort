8510a82f45f84254c76b84c8f23e310b589ecf81
Brian Hicks <brian@brianthicks.com>
2021-08-30 05:43:35 -0500

    add an overlay

--- flake.nix
+++ flake.nix
@@ -17,6 +17,10 @@
         packages.similar-sort = naersk-lib.buildPackage ./.;
         defaultPackage = packages.similar-sort;
 
+        overlay = final: prev: {
+          similar-sort = packages.similar-sort;
+        };
+
         # `nix run`
         apps.similar-sort =
           flake-utils.lib.mkApp { drv = packages.similar-sort; };
